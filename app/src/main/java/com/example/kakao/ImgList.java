87ad4e6791fb798723963885c9bbf2853afbf959
이현규\Hyun Gyu <dlgusrb0813@gmail.com>
2020-04-21 13:19:38 +0900

    temp

--- app/src/main/java/com/example/kakao/ImgList.java
+++ app/src/main/java/com/example/kakao/ImgList.java
@@ -19,4 +19,10 @@ public class ImgList {
         for(int i=0; i<200;i++)
         data.add("https://www.gettyimagesgallery.com/wp-content/uploads/2018/12/GettyImages-3360502.jpg");
     }
+
+    public void temp(){
+
+
+    }
+
 }
--- app/src/main/java/com/example/kakao/MainActivity.java
+++ app/src/main/java/com/example/kakao/MainActivity.java
@@ -6,17 +6,91 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
+    private final  String TAG = MainActivity.class.getSimpleName();
     private RecyclerView rv;
     private ImgList imgList;
+
+
+    private WebView webView;
+    private String url = "https://www.gettyimagesgallery.com/collection/sasha/";
+    private String source;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+
+        webView = findViewById(R.id.web);
+//        webView.getSettings().setJavaScriptEnabled(true);
+//        webView.loadUrl(url);
+//        webView.setWebChromeClient(new WebChromeClient());
+//        webView.setWebViewClient(new WebViewClientClass());
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 자바스크립트인터페이스 연결
+        // 이걸 통해 자바스크립트 내에서 자바함수에 접근할 수 있음.
+        webView.addJavascriptInterface(new MyJavascriptInterface(), "Android");
+        // 페이지가 모두 로드되었을 때, 작업 정의
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // 자바스크립트 인터페이스로 연결되어 있는 getHTML를 실행
+                // 자바스크립트 기본 메소드로 html 소스를 통째로 지정해서 인자로 넘김
+                view.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('body')[0].innerHTML);");
+            }
+        });
+        //지정한 URL을 웹 뷰로 접근하기
+        webView.loadUrl(url);
+
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("https://www.gettyimagesgallery.com/collection/sasha/");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET"); //전송방식
+//                    connection.setDoOutput(true);       //데이터를 쓸 지 설정
+                    connection.setDoInput(true);        //데이터를 읽어올지 설정
+                    connection.setConnectTimeout(10000);
+                    InputStream is = connection.getInputStream();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
+                    String result;
+                    while((result = br.readLine())!=null){
+                        sb.append(result+"\n");
+                    }
+
+                    result = sb.toString();
+                    Log.e(TAG,result);
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+
     }
 
     @Override
@@ -42,6 +116,34 @@ public class MainActivity extends AppCompatActivity {
         rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         rv.setAdapter(new RvAdapter(imgList.getData(), this));
     }
+//    @Override
+//    public boolean onKeyDown(int keyCode, KeyEvent event) {
+//        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+//            webView.goBack();
+//            return true;
+//        }
+//        return super.onKeyDown(keyCode, event);
+//    }
+
+
+    public class MyJavascriptInterface {
+        @JavascriptInterface
+        public void getHtml(String html) {
+            //위 자바스크립트가 호출되면 여기로 html이 반환됨
+            source = html;
+            Log.e(TAG,source);
+        }
+    }
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+
 }
 
 

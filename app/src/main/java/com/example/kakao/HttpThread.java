f6254249ca0b6498562c1420696d23736e4ee11e
unknown <dlgusrb0813@gmail.com>
2020-04-21 22:37:17 +0900

    기능완성

--- app/src/main/java/com/example/kakao/HttpThread.java
+++ app/src/main/java/com/example/kakao/HttpThread.java
@@ -1,18 +1,90 @@
 package com.example.kakao;
 
+import android.os.Looper;
 import android.util.Log;
 
-public class HttpThread implements Runnable {
-    final private String TAG = HttpThread.class.getSimpleName();
-    ImgList imgList;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
-    public HttpThread(ImgList imgList) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+public class HttpThread extends Thread {
+    final private String TAG = HttpThread.class.getSimpleName();
+    final static String URL = "https://www.gettyimagesgallery.com/collection/sasha/";
+    final static String FIND = "data-src=\"";
+
+    private ArrayList<String> imgList;
+    private RvAdapter adapter;
+    private Handler handler;
+    private TextView textView;
+
+    public HttpThread(ArrayList<String> imgList, RvAdapter adapter, Handler handler, TextView textView) {
         this.imgList = imgList;
+        this.adapter = adapter;
+        this.handler = handler;
+        this.textView = textView;
     }
 
     @Override
     public void run() {
-        imgList.getImgList();
-        Log.e(TAG,((Integer)imgList.getData().size()).toString());
+        if(BuildConfig.DEBUG) Log.e(TAG,"run");
+        getImgList();
+    }
+
+    private void getImgList() {
+        if(BuildConfig.DEBUG) Log.e(TAG,"getImgList");
+
+        //try-with-resource로 바꾸기
+        try {
+            java.net.URL targetUrl = new URL(URL);
+            HttpURLConnection connection = (HttpURLConnection) targetUrl.openConnection();
+            connection.setRequestMethod("GET"); //전송방식
+            connection.setDoInput(true);        //데이터를 읽어올지 설정
+            InputStream is = connection.getInputStream();
+//            StringBuilder sb = new StringBuilder();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+
+            String result;
+            boolean isPosted = false;
+            while ((result = br.readLine()) != null) {
+
+                int start = result.indexOf(FIND);
+                if (start == -1) continue;
+                start += FIND.length();
+                imgList.add(result.substring(start, result.indexOf("\"", start + 1)));
+                if(!isPosted &&imgList.size()>5) {
+                    postImgList();
+                    isPosted =true;
+                }
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        if(BuildConfig.DEBUG) Log.e(TAG, "로드된 이미지 개수: " + ((Integer) imgList.size()).toString());
+    }
+
+    private void postImgList(){
+        if(BuildConfig.DEBUG) Log.e(TAG,"postImgList, imgList.size() : "+((Integer)imgList.size()).toString());
+
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                textView.setVisibility(View.GONE);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kakao/MainActivity.java
+++ app/src/main/java/com/example/kakao/MainActivity.java
@@ -6,140 +6,51 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.widget.TextView;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
     private final  String TAG = MainActivity.class.getSimpleName();
     private RecyclerView rv;
-    private ImgList imgList;
+    private RvAdapter adapter;
+    private ArrayList<String>imgList;
 
-
-    private WebView webView;
-    private String url = "https://www.gettyimagesgallery.com/collection/sasha/";
-    private String source;
+    private TextView textView;
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-
-        webView = findViewById(R.id.web);
-//        webView.getSettings().setJavaScriptEnabled(true);
-//        webView.loadUrl(url);
-//        webView.setWebChromeClient(new WebChromeClient());
-//        webView.setWebViewClient(new WebViewClientClass());
-
-
-        webView.getSettings().setJavaScriptEnabled(true);
-        // 자바스크립트인터페이스 연결
-        // 이걸 통해 자바스크립트 내에서 자바함수에 접근할 수 있음.
-        webView.addJavascriptInterface(new MyJavascriptInterface(), "Android");
-        // 페이지가 모두 로드되었을 때, 작업 정의
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                // 자바스크립트 인터페이스로 연결되어 있는 getHTML를 실행
-                // 자바스크립트 기본 메소드로 html 소스를 통째로 지정해서 인자로 넘김
-                view.loadUrl("javascript:window.Android.getHtml(document.getElementsByTagName('body')[0].innerHTML);");
-            }
-        });
-        //지정한 URL을 웹 뷰로 접근하기
-        webView.loadUrl(url);
-
-        new Thread(){
-            @Override
-            public void run() {
-                try {
-                    URL url = new URL("https://www.gettyimagesgallery.com/collection/sasha/");
-                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET"); //전송방식
-//                    connection.setDoOutput(true);       //데이터를 쓸 지 설정
-                    connection.setDoInput(true);        //데이터를 읽어올지 설정
-                    connection.setConnectTimeout(10000);
-                    InputStream is = connection.getInputStream();
-                    StringBuilder sb = new StringBuilder();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(is,"UTF-8"));
-                    String result;
-                    while((result = br.readLine())!=null){
-                        sb.append(result+"\n");
-                    }
-
-                    result = sb.toString();
-                    Log.e(TAG,result);
-
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-
-       new Thread(new Runnable() {
-            @Override
-            public void run() {
-                imgList.getImgList();
-            }
-        }).run();
+        if(BuildConfig.DEBUG) Log.e(TAG,"onStart");
+        HttpThread ht = new HttpThread(imgList,adapter,mHandler,textView);
+        ht.start();
 
     }
 
     private void init(){
-        imgList = new ImgList();
 
+        imgList = new ArrayList<>();
 
+        textView = findViewById(R.id.noti);
+
+        mHandler = new Handler();
         //recycler view
         rv = findViewById(R.id.recylcer_view);
         rv.addItemDecoration(new DividerItemDecoration(getBaseContext(), 1));
         rv.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
-        rv.setAdapter(new RvAdapter(imgList.getData(), this));
-    }
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
-//            webView.goBack();
-//            return true;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-
-
-    public class MyJavascriptInterface {
-        @JavascriptInterface
-        public void getHtml(String html) {
-            //위 자바스크립트가 호출되면 여기로 html이 반환됨
-            source = html;
-            Log.e(TAG,source);
-        }
-    }
-    private class WebViewClientClass extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+        adapter = new RvAdapter(imgList,this);
+        rv.setAdapter(adapter);
     }
 
 
